1a6e046f6a81f0796292cff4fcdeda64e46086cd
Vitaly Kazantsev <80334205+kazantsev-v@users.noreply.github.com>
2024-07-19 18:03:33 +0500

    alpha 2

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require(`express`);
+const app = express();
+const morgan = require("morgan");
+const dotenv = require("dotenv").config();
+const port = process.env.PORT || 4499;
+
+const https = require('node:https');
+const fs = require('node:fs');
+
+const certDir = `/etc/letsencrypt/live`;
+const domain = `why13idontknow.hopto.org`;
+
+app.use(morgan("dev"));
+app.use(express.static(`public`));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("WELCOME TO THE BASIC EXPRESS APP WITH AN HTTPS SERVER");
+});
+
+const options = {
+  key: fs.readFileSync(`${certDir}/${domain}/privkey.pem`),
+  cert: fs.readFileSync(`${certDir}/${domain}/fullchain.pem`)
+};
+
+const server = https.createServer(options, app);
+
+server.listen(port, () => {
+  console.log(`App listening on https://localhost:${port}`);
+});
+
+// Настройка БД
+let mongoose = require('mongoose');
+mongoose.connect('mongodb://91.108.243.98:27017/tgweb', {
+    authSource: "admin",
+    user: "olegsky",
+    pass: "Cat190808_190808!"
+});
+
+// Схемы
+let usersSchema = new mongoose.Schema({
+    userid: String,
+    theme: String
+});
+
+let User = mongoose.model('users', usersSchema);
+
+// Профиль пользователя
+app.get(`/search`, async function (req, res) {
+    console.log("yesssssssssssssssssssssssssssssss")
+    let theme = req.query.theme;
+    console.log(theme)
+    let user = await User.findOne({theme: theme});
+    console.log(user)
+    res.send(user);
+});
---
+++ public/script.js
@@ -0,0 +1,22 @@
+let tg = window.Telegram.WebApp
+tg.initData
+Telegram.WebApp.ready();
+
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+
+let theme = document.querySelector(`.theme`)
+let search = document.querySelector(`.search`)
+let userText = document.querySelector(`.text`)
+let userFind = document.querySelector(`.user-find`)
+let buttons = document.querySelectorAll(`.button`)
+
+search.addEventListener('click', async function () {
+    buttons[0].classList.add('hide')
+    buttons[1].classList.add('hide')
+    select = theme.value;
+    console.log(select)
+    let response = await axios.get(`/search`, { params: { theme: select } });
+    USER = response.data;
+    console.log(USER);
+    userFind.innerHTML = `Найден пользователь <a href="https://t.me/${USER.userid}">Name</a>`
+});
